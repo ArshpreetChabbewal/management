7640f052322ba7d7fb1c560b81a54386adc8cf98
Arshpreet Chabbewal <arsh.c321@gmail.com>
2022-08-29 13:54:39 -0400

    implement theme settings

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ import "./App.css";
 import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
 
   return (
     <div>
@@ -38,6 +38,7 @@ const App = () => {
               <button
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                onClick={() => setThemeSettings(true)}
                 style={{ background: "blue", borderRadius: "50%" }}
               >
                 <FiSettings />
@@ -62,7 +63,7 @@ const App = () => {
               <Navbar />
             </div>
             <div>
-              <ThemeSettings />
+              {themeSettings && <ThemeSettings />}
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,21 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
+
+  const setMode = (e) => {
+    setCurrrentMode(e.target.value);
+
+    localStorage.setItem("themeMode", e.target.value);
+  };
+
+  const setColor = (e) => {
+    setCurrrentColor(e.target.value);
+
+    localStorage.setItem("colorMode", e.target.value);
+  };
 
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
@@ -26,6 +41,11 @@ export const ContextProvider = ({ children }) => {
         isClicked: isClicked,
         setIsClicked: setIsClicked,
         handleClick: handleClick,
+        currentColor: currentColor,
+        currentMode: currentMode,
+        setCurrentColor: setCurrentColor,
+        setCurrentMode: setCurrentMode,
+        themeSettings: themeSettings,
       }}
     >
       {children}
